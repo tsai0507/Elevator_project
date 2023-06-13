0c07abc50ed74d695b4b3064c54afa7f6cfe7ba3
tsai0507 <frank4144@gmail.com>
2023-06-13 14:45:48 +0800

    0613

--- elevator_simulator/.ele_driver.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/leo/EOS/final/combine0611_rpi/ele_driver.mod := { echo  /home/leo/EOS/final/combine0611_rpi/ele_driver.o;  echo; } > /home/leo/EOS/final/combine0611_rpi/ele_driver.mod
--- elevator_simulator/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/leo/EOS/final/combine0611_rpi/modules.order := {   echo /home/leo/EOS/final/combine0611_rpi/ele_driver.ko; :; } | awk '!x[$$0]++' - > /home/leo/EOS/final/combine0611_rpi/modules.order
